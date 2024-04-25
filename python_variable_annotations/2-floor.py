c065868448080a89b8d05c60c59a63ddfbae1559
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 15:45:34 +0200

    Update length comment

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" A type-annotated function floor that takes a float n as argument and returns the floor of the float """
+""" A type-annotated function floor that takes a float n as argument """
 
 import math  # import the math module
 
