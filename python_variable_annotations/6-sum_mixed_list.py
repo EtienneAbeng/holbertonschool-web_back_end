3dfcbec2dc64574c1ab7af72e829d554c9a86c98
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 16:13:21 +0200

    Update code with Union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+""" A type-annotated function sum_mixed_list with list input of mixed """
+from typing import List, Union
 
-import typing
 
-
-def sum_mixed_list(mxd_lst: "typing.List[typing.Union[int, float]]") -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Return the sum of the list of floats and integers"""
     return sum(mxd_lst)
