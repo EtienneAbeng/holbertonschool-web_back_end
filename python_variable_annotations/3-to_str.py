a791480245194fa0bb23e72849173cce24a109be
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 11:49:58 +0200

    a type-annotated function to_str that takes a float n

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" A type-annotated function to_str that takes a float n as argument """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the float n """
+    return str(n)
