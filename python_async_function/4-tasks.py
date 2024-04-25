24ac5a20b594e715be2fc599577b7925b5a7cc51
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 15:07:16 +0200

    Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called.

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,31 @@
+import asyncio
+from typing import List
+from asyncio import Task
+
+""" Tasks """
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Invoque task_wait_random n fois avec le max_delay spécifié.
+    Renvoie la liste de tous les délais (valeurs float).
+    La liste des délais doit être dans l'ordre croissant sans utiliser sort() en raison de la concurrence.
+    """
+    # Liste pour stocker les délais
+    delays = []
+
+    # Crée des tâches pour attendre un délai aléatoire entre 0 et max_delay (inclus)
+    # et les stocke dans une liste
+    tasks = [task_wait_random(max_delay) for index in range(n)]
+
+    # Attend que toutes les tâches soient terminées et collecte leurs résultats
+    for task in asyncio.as_completed(tasks):
+        # Attend la fin de la tâche et récupère le résultat
+        delay = await task
+        # Ajoute le délai à la liste des délais
+        delays.append(delay)
+
+    # Renvoie les délais dans l'ordre croissant
+    return delays
