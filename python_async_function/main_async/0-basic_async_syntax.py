d584c386e25eb440f8c846aa79c4aa37e6bc668b
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 13:03:07 +0200

    Let's execute multiple coroutines at the same time with async

---
+++ python_async_function/main_async/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import asyncio  # Importer le module asyncio pour la programmation asynchrone
+import random   # Importer le module random pour générer des nombres aléatoires
+
+async def wait_random(max_delay: int = 10) -> float:
+    # Générer un délai aléatoire entre 0 et max_delay secondes
+    random_delay = random.uniform(0, max_delay)
+    
+    # Attendre le délai aléatoire
+    await asyncio.sleep(random_delay)
+    
+    # Renvoyer le délai aléatoire
+    return random_delay
