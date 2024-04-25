579461821bb18a6dc81dcbfe1378802c20d970c2
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 16:09:14 +0200

    Update comment below shebang

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 
 import typing
 
+
 def sum_mixed_list(mxd_lst: "typing.List[typing.Union[int, float]]") -> float:
     """ Return the sum of the list of floats and integers"""
     return sum(mxd_lst)
