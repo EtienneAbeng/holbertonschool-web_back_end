4c079406d8b508fcd9f73ff1edf4d9e8c30e6a2a
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 16:31:44 +0200

    update code with autopep

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ A type-annotated function sum_mixed_list with list input of mixed """
 
-from typing import List, Union
 
+from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Return the sum of the list of floats and integers """
