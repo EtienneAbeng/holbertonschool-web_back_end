788efe61bda165281f0fa57ee0621ee4a352c209
EtienneAbeng <abengetienne@gmail.com>
2024-04-25 15:52:14 +0200

    Update comment below shebang

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,7 +1,8 @@
 #!/bin/usr/env python3
 """ Define and annotate the following variables with the specified values """
 
-a: int = 1 # an integer
-pi: float = 3.14 # a float
-i_understand_annotations: bool = True # a boolean
-school: str = "Holberton" # a string
+
+a: int = 1  # an integer
+pi: float = 3.14  # a float
+i_understand_annotations: bool = True  #  a boolean
+school: str = "Holberton"  #  a string
