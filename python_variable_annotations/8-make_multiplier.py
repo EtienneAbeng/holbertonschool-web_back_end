f399c22e1e1a214950de703ecdc23f15d90cc81b
EtienneAbeng <abengetienne@gmail.com>
2024-04-26 11:51:15 +0200

    Update code with comment less long

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-""" A type-annotated function make_multiplier that takes 
-a float multiplier as argument"""
+"""Complex types - functions"""
 
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """ Return a function that multiplies a float by multiplier"""
-    def multiplier_function(number: float) -> float:
-        return number * multiplier
+    """Returns a function that multiplies a float by the multiplier."""
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
     return multiplier_function
