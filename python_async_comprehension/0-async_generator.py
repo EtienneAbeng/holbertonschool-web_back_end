a01795d5f2cdba3aeeb9961b29723332fba182d0
EtienneAbeng <abengetienne@gmail.com>
2024-04-26 23:13:41 +0200

    add typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,17 +8,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Coroutine that generates 10 random numbers between 0 and 10."""
-    for index in range(10):
-        await sleep(1)  # Attente asynchrone de 1 seconde
-        yield uniform(0, 10)  # Renvoie un nombre aléatoire entre 0 et 10
-
-
-async def print_yielded_values():
-    """Function that consumes the values produced by async_generator
-       and prints them."""
-    result = [number async for number in async_generator()]
-    print(result)
-
-# Exécution de la coroutine print_yielded_values
-asyncio.run(print_yielded_values())
+    """Coroutine that generate 10 random number enter 0 to 10."""
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
