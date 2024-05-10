b3bf75f3a1ae0f0e48208c5474ead5b1023eee84
EtienneAbeng <abengetienne@gmail.com>
2024-05-10 22:51:59 +0200

    Update code after correction whitespace, etc...

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -1,16 +1,15 @@
 export default function getSanFranciscoDescription() {
-    // Définition des variables
-    const year = 2017;
-    const budget = {
-      income: "$119,868",
-      gdp: "$154.2 billion",
-      capita: "$178,479",
-    };
-  
-    /* Utilisation d'un littéral de modèle pour construire la chaîne de 
+  // Définition des variables
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  /* Utilisation d'un littéral de modèle pour construire la chaîne de
       caractères avec les variables insérées dynamiquement */
-    return `As of ${year}, it was the seventh-highest income county in the United States, 
+  return `As of ${year}, it was the seventh-highest income county in the United States, 
       with a per capita personal income of ${budget.income}. As of 2015, 
       San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
-  }
-  
+}
