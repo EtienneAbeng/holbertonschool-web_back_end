b6e9c965c355f202bc37b61acf955b3db9cdbc2e
EtienneAbeng <abengetienne@gmail.com>
2024-05-14 13:11:19 +0200

    Implement a class named ClassRoom:
    Prototype: export default class ClassRoom
    It should accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// Définition de la classe Classroom
+export default class Classroom {
+    // Constructeur de la classe Classroom
+    constructor(maxStudentsSize) {
+        // Initialisation de la propriété maxStudentsSize avec la valeur passée en paramètre
+        this.maxStudentsSize = maxStudentsSize;
+    };
+};
