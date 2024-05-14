8cba4a3dfbc43d85b7753ede8fd983b25b7d8af9
EtienneAbeng <abengetienne@gmail.com>
2024-05-14 21:51:41 +0200

    Update comment

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,8 +1,8 @@
 // Définition de la classe Classroom
 export default class Classroom {
-    // Constructeur de la classe Classroom
-    constructor(maxStudentsSize) {
-        // Initialisation de la propriété maxStudentsSize avec la valeur passée en paramètre
-        this.maxStudentsSize = maxStudentsSize;
-    };
-};
+// Constructeur de la classe
+  constructor(maxStudentsSize) {
+  // Initialisation de la propriété maxStudentsSize avec la valeur passée en paramètre
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
