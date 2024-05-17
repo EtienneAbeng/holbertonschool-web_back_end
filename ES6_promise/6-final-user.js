b8e6b6fd10a37717700b44f5b716f6274152b74c
EtienneAbeng <abengetienne@gmail.com>
2024-05-17 20:39:19 +0200

    Write and export a function named handleProfileSignup. It should accept three arguments firstName (string), lastName (string), and fileName (string). The function should call the two other functions. When the promises are all settled it should return an array with the following structure:

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,20 +1,19 @@
-// Importer les fonctions signUpUser et uploadPhoto
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-// Définir la fonction handleProfileSignup
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Appeler les fonctions signUpUser et uploadPhoto avec les arguments spécifiés
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  // Attendre que les deux promesses soient résolues ou rejetées
-  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
-    // Créer un tableau contenant les résultats des promesses
-    results.map((result) =>
-      // Pour chaque résultat, retourner un objet avec le statut et la valeur ou l'erreur
-      ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      })));
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
 }
