ccc5966e971401405b414bc808ece3a4369642d5
EtienneAbeng <abengetienne@gmail.com>
2024-05-17 19:13:06 +0200

    Return a Promise using this prototype function getResponseFromAPI()

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,4 @@
-// Définir la fonction getResponseFromAPI
-function getResponseFromAPI() {
-  // Retourner une nouvelle promesse
-  return new Promise((resolve, reject) => {});
+/* eslint-disable */
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
 }
-
-// Exporter la fonction getResponseFromAPI comme module par défaut
-export default getResponseFromAPI;
